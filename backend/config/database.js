b9b0158bf933c45eed06bdf4b8132f990f8699c2
IshaanSammi <ishaansammi004@gmail.com>
2025-06-23 14:06:59 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 const connectDB = async () => {
-   await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/chatapp2").then(() => {
+   await mongoose.connect(process.env.MONGO_URI ).then(() => {
     console.log('MongoDB connected');
    }).catch((error) => {
     console.log(error)
