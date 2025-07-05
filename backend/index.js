21deac5d2d1ec2db151d4081ba4480fda91ba5ca
Ishaan Sammi <ishaansammi004@gmail.com>
2025-07-05 13:23:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config({});
  
 const PORT = process.env.PORT || 8000;
 
-// middleware
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
@@ -23,7 +23,7 @@ const corsOption={
 app.use(cors(corsOption)); 
 
 
-// routes
+
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
  
